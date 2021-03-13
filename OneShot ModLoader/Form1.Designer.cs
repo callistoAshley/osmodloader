8b4bf4f132f79733aa84f456f2e6b75c0d53993e
Samuel J. Newsome <mastersamuelj@gmail.com>
2021-03-13 15:51:51 +1100

    ui

--- OneShot ModLoader/Form1.Designer.cs
+++ OneShot ModLoader/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(838, 450);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "OneShot ModLoader";
