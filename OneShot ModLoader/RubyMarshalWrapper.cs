30263b0c64b3ee9134512d387924e502d8c29ace
callistoAshley <callistoashley@proton.me>
2022-01-06 15:41:50 +1100

    import kaitai struct and a ruby marshal implimentation
    i have no idea if this will work!

---
+++ OneShot ModLoader/RubyMarshalWrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using OneShot_ModLoader.Kaitai;
+
+// so this is a ruby marshal parser implimentation in something called kaitai
+// kaitai is... a thing?
+// to the peak of my understanding it looks like a parser to parse other parsers in a yaml-looking format
+// here's where i found it if you want to look at it yourself: https://formats.kaitai.io/ruby_marshal/index.html
+// i'm still kinda tweaking this and learning how it works so it won't come out in the next update
+
+namespace OneShot_ModLoader
+{
+    public static class RubyMarshalWrapper
+    {
+        public static RubyMarshal FromFile(string path)
+        {
+            return RubyMarshal.FromFile(path);
+        }
+    }
+}
