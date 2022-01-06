e22b0c89ca3f8e3e86e85ce68a90b191a76bf498
Samuel J. Newsome <mastersamuelj@gmail.com>
2022-01-06 14:56:27 +1100

    very serious commit message

---
+++ OneShot ModLoader/ScriptUnpacker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace OneShot_ModLoader
+{
+    public static class ScriptUnpacker
+    {
+        // need to find a working marshal parser :/
+        public static void Unpack(string inputPath, string outputPath)
+        {
+            
+        }
+    }
+}
